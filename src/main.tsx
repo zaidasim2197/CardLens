d5053f1c86a7d203e367666afd35797b0fc6f3fa
Ibrahim Ahmed <iamalik2005@gmail.com>
2026-09-19 13:52:06 +0500

    changed color theme and fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource-variable/plus-jakarta-sans'
+import '@fontsource/poppins/latin-400.css'
+import '@fontsource/poppins/latin-500.css'
+import '@fontsource/poppins/latin-600.css'
+import '@fontsource/poppins/latin-700.css'
+import '@fontsource/poppins/latin-800.css'
 import App from './App.tsx'
 import './index.css'
 import { Toaster } from "@/components/ui/sonner"
